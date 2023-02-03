e2716fc800d1aeed22601cf992020d73d0b28f67
Rishabh Prakash <67912473+7thripr@users.noreply.github.com>
2023-02-03 14:46:00 +0000

    initial: first com
    hello world

---
+++ install.sh
@@ -0,0 +1,28 @@
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Install nala
+apt install nala -y
+
+# Tasksel to install required GUI
+nala install tasksel -y
+
+# Install brave-browser
+sudo nala install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo nala update
+sudo nala install brave-browser -y
+
+#Set taget to graphical
+systemctl set-default graphical.target
